39c350fab613af55d41670937726c9249cb86c11
Muneeb Sheikh <muneikh@gmail.com>
2016-07-13 02:24:24 +0500

    removed toggle view

--- app/src/main/java/com/muneikh/ui/CameraActivity.java
+++ app/src/main/java/com/muneikh/ui/CameraActivity.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 
 import com.muneikh.inputsource.CameraTextureVideoInputSource;
 import com.muneikh.inputsource.TextureVideoInputSource;
@@ -29,14 +27,12 @@ public class CameraActivity extends Activity implements TextureVideoInputSourceE
     private TextureVideoInputSource textureVideoInputSource;
     private GL2SurfaceView glSurfaceView;
     private VideoRenderer videoRenderer;
-    private Switch cameraToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         glSurfaceView = (GL2SurfaceView) findViewById(R.id.gl_surface_view);
-        cameraToggle = (Switch) findViewById(R.id.toggle);
 
         if (BuildConfig.AUTO_TEST) {
             this.textureVideoInputSource = new VideoFileInputSource(this, "mock_input_video.mp4");
